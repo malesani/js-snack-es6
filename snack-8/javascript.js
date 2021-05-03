4a5274859552d847375ccf9917d9b76393429249
malesani <alejandroMalesani@hotmail.com>
2021-05-03 16:05:38 +0200

    second

--- snack-8/javascript.js
+++ snack-8/javascript.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
   
+  stardNomi = [
+    "PLUTO",
+    "MICKEY",
+    "DEISY",
+    "GOOFY",
+    "DONALD"
+  ]
+
+  const finishNomi = stardNomi.map(function(nome){
+    return changeName(nome)
+  }) 
+
+  console.log(finishNomi)
 
 
-
-
+  function changeName(nome){
+   return nome.toLowerCase()
+  }
 })
